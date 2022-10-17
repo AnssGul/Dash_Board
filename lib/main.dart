66b2e85750ca199e63f0cbea677cc23972ce380a
muhammadanssguljalal@gmail.com <anssgul1>
2022-10-17 09:15:36 -0700

    Dish Board App

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,10 @@
 //import 'package:dash_board/model/pages/DashBoard.dart';
 //import 'package:dash_board/model/pages/Loginpage.dart';
 //import 'package:dash_board/model/pages/SignUp.dart';
-import 'package:dash_board/model/pages/Loginpage.dart';
 import 'package:dash_board/model/pages/interested.dart';
 import 'package:flutter/material.dart';
 
-//import 'model/pages/signUp2.dart';
-import 'model/pages/DashBoard.dart';
-import 'model/pages/allergy_information.dart';
-import 'model/pages/signUp2.dart';
-import 'model/pages/signUp3.dart';
-import 'model/pages/user_currentlocation.dart';
-
-void main() => runApp(
-
- const      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home:InterestedThing(),
-      )
-
-);
-
-
-
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InterestedThing(),
+    ));
